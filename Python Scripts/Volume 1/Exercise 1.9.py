bc64b6970024f3210109294cf463acbe8075610b
Adam Crowe <crowead@tcd.ie>
2024-05-27 17:10:19 +0100

    Vol 1, Chap 1, Exer 1.9

---
+++ Python Scripts/Volume 1/Exercise 1.9.py
@@ -0,0 +1,27 @@
+NEXT_STOCK_PRICE_HEADS = lambda sp: sp+10
+NEXT_STOCK_PRICE_TAILS = lambda sp: sp-10
+STRIKE_PRICE = 80
+PAYOFF_PERIOD = 5
+PAYOFF = lambda sp: max(sp - STRIKE_PRICE, 0)
+
+history = {}
+
+def derivative_price(period: int, stock_price: float) -> float:
+    price: float
+    key = f"period: {period}, stock_price: {stock_price}"
+
+    if key in history:
+        price = history[key]
+    elif period == PAYOFF_PERIOD:
+        price = PAYOFF(stock_price)
+    else:
+        derivative_price_next_period_heads = derivative_price(period + 1, NEXT_STOCK_PRICE_HEADS(stock_price))
+        derivative_price_next_period_tails = derivative_price(period + 1, NEXT_STOCK_PRICE_TAILS(stock_price))
+        price = 0.5 * (derivative_price_next_period_heads + derivative_price_next_period_tails)
+
+    history[key] = price
+    return price
+
+if __name__ == "__main__":
+    answer = derivative_price(0, 80)
+    print(f"v_0(80) = {answer}")
